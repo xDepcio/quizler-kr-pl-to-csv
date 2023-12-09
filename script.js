15bfa8707507c2b61d77a4386632e50dbacf79bc
Aleksander Drwal <107278893+xDepcio@users.noreply.github.com>
2023-12-09 14:34:53 +0100

    Update script.js
    changed script to escape commas

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
     const termsPl = Array.from(document.getElementsByClassName('lang-pl'))
   const csv = [];
     const zipped = termsKo.map((e, i) => ([e.innerText, termsPl[i].innerText]))
-    zipped.forEach(term => {csv.push(`${term[0]},${term[1]}`)})
+    zipped.forEach(term => {csv.push(`"${term[0]}","${term[1]}"`)})
 
 
   console.log(csv.join('\n'));
